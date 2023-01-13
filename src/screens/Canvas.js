d352e4b8c75fa0474ff7b9c0781cfe5f0d309fd9
Vaibhav Shah <user1@extendeditarms.com>
2023-01-13 17:43:01 +0530

    canvas changes

--- src/screens/Canvas.js
+++ src/screens/Canvas.js
@@ -9,46 +9,64 @@ import {
   StyleSheet,
 } from 'react-native';
 import {DragResizeBlock} from 'react-native-drag-resize';
+import { IconButton, MD3Colors } from 'react-native-paper';
 
 const CanvasScreen = () => {
-  const pan = useRef(new Animated.ValueXY()).current;
+  // const pan = useRef(new Animated.ValueXY()).current;
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
-      onPanResponderRelease: () => {
-        pan.extractOffset();
-      },
-    }),
-  ).current;
+  // const panResponder = useRef(
+  //   PanResponder.create({
+  //     onMoveShouldSetPanResponder: () => true,
+  //     onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
+  //     onPanResponderRelease: () => {
+  //       pan.extractOffset();
+  //     },
+  //   }),
+  // ).current;
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <DragResizeBlock x={0} y={0}>
-          {/* <Animated.View
-          style={{
-            transform: [{translateX: pan.x}, {translateY: pan.y}],
-          }}
-        {...panResponder.panHandlers}> */}
-          <View style={styles.box} />
-          <Text style={styles.titleText}>Drag this box!</Text>
-          {/* </Animated.View> */}
-        </DragResizeBlock>
+    <SafeAreaView style={styles.safeAreaCotainer}>
+      <View style={styles.canvasArea}>
+        <View style={styles.container}>
+          <DragResizeBlock x={0} y={0}>
+            <View style={styles.box} />
+            <Text style={styles.titleText}>Drag this box!</Text>
+          </DragResizeBlock>
+        </View>
       </View>
-      <View style={styles.squreButton}>
-        <Button title="Square"></Button>
+      <View style={styles.componentArea}>
+        <View style={styles.squreButton}>
+          <Button title="Square"></Button>
+          <IconButton
+            icon="camera"
+            iconColor={MD3Colors.error50}
+            size={20}
+            onPress={() => console.log('Pressed')}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeAreaCotainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  canvasArea: {
+    alignSelf: 'center',
+    // backgroundColor: 'grey'
+  },
+  componentArea: {
+    backgroundColor: 'grey',
+    width: '100%',
+    alignSelf: 'flex-end',
   },
   titleText: {
     fontSize: 14,
@@ -64,9 +82,8 @@ const styles = StyleSheet.create({
   squreButton: {
     height: 40,
     width: 100,
-    alignSelf: 'flex-end'
+    alignSelf: 'flex-end',
   },
 });
 
 export default CanvasScreen;
-  
